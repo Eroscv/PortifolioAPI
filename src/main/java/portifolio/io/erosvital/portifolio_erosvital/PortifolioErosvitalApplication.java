ee82a1c7f1cabe3f4520fd5ea597b7277b45804c
Eroscv <erosvital9@gmail.com>
2024-01-03 13:50:53 -0300

    Portifolio 2

---
+++ src/main/java/portifolio/io/erosvital/portifolio_erosvital/PortifolioErosvitalApplication.java
@@ -0,0 +1,13 @@
+package portifolio.io.erosvital.portifolio_erosvital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioErosvitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioErosvitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portifolio/io/erosvital/portifolio_erosvital/PortifolioErosvitalApplicationTests.java
@@ -0,0 +1,13 @@
+package portifolio.io.erosvital.portifolio_erosvital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioErosvitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
